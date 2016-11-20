1e8d85cd55660bdca72785a6bf7bb529ca8d10a4
Eddie Antonio Santos <easantos@ualberta.ca>
2016-11-20 14:44:42 -0700

    Remove string extractor since its functionality is provided by Esprima.

--- index.js
+++ index.js
@@ -17,36 +17,3 @@
 exports.SourceFile = require('./lib/source-file');
 exports.Grammar = require('./lib/grammar');
 exports.GrammarRegistry = require('./lib/grammar-registry');
-
-/**
- * Converts string literals into normalized strings.
- */
-exports.extractString = (literal) => {
-  let match;
-
-  /* Matches single-quoted strings, double-quouted strings, and "standalone"
-   * template literals (without any interpolation). */
-  if (match = literal.match(/^([`'"])((?:\s|.)*)\1$/)) {
-    let [_, _quote, contents] = match;
-    return interpretEscapes(contents);
-  }
-
-  throw new Error(`Could not parse literal ${literal}`);
-
-  function interpretEscapes(string) {
-    return string.replace(/\\(\w)/g, (_, code) => {
-      /* http://www.ecma-international.org/ecma-262/7.0/#prod-SingleEscapeCharacter */
-      return {
-        "'": "'",
-        '"': '"',
-        "\\": "\\",
-        "b": "\b",
-        "f": "\f",
-        "n": "\n",
-        "r": "\r",
-        "t": "\t",
-        "v": "\v"
-      }[code];
-    });
-  }
-};
