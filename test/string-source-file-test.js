e9104533231687b7000bec8fce0584cf012db943
Eddie Antonio Santos <easantos@ualberta.ca>
2016-11-20 15:25:36 -0700

    Parse strings from source files!

--- test/string-source-file-test.js
+++ test/string-source-file-test.js
@@ -15,23 +15,13 @@
  */
 
 import test from 'ava';
-import {NotImplementedError} from 'common-errors';
 
 import {SourceFile} from '../';
 
+test('SourceFileFromString#strings()', t => {
+  const sourceFile = SourceFile.fromString(`
+    "\\n\\t"
+  `);
 
-test('SourceFile#hash', t => {
-  t.throws(() => new SourceFile().hash, NotImplementedError);
-});
-
-test('SourceFile#ast', t => {
-  t.throws(() => new SourceFile().ast, NotImplementedError);
-});
-
-test('SourceFile#tokens', t => {
-  t.throws(() => new SourceFile().tokens, NotImplementedError);
-});
-
-test('SourceFile#strings()', t => {
-  t.throws(() => new SourceFile().strings(), NotImplementedError);
+  t.deepEqual(Array.from(sourceFile.strings()), ["\n\t"]);
 });
