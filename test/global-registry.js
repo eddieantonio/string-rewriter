a10eaa3dd97dfe2d24d0b3f1e32da33d4af602b4
Eddie Antonio Santos <easantos@ualberta.ca>
2016-11-20 17:06:12 -0700

    Add support for URIs.

--- test/global-registry.js
+++ test/global-registry.js
@@ -18,18 +18,32 @@ import test from 'ava';
 
 import {globalRegistery} from '../';
 
-test('parses ISO dates', t => {
+test('parses ISO 8601 dates', t => {
   const date = globalRegistery.get('iso8601-date');
 
-  t. true(date.parse('1992-02-27'));
-  t. true(date.parse('1992-02-27'));
+  t.true(date.parse('1992-02-27'));
+  t.true(date.parse('1992-02-27'));
   t.false(date.parse('1992-02-27T13:00:00'));
 });
 
-test('parses ISO datetimes', t => {
+test('parses ISO 8601 datetimes', t => {
   const datetime = globalRegistery.get('iso8601-datetime');
-  t.false(datetime.parse('1992-02-27'));
-  t. true(datetime.parse('1992-02-27T13:00:00'));
+
+  t.true(datetime.parse('1992-02-27T13:00:00'));
+  t.true(datetime.parse('1992-02-27T13:00:00-06:00'));
+  t.true(datetime.parse('1992-02-27T13:00:00Z'));
   t.false(datetime.parse('1992-02-27T13:00:00-'));
-  t. true(datetime.parse('1992-02-27T13:00:00-06:00'));
+  t.false(datetime.parse('1992-02-27'));
+});
+
+test('parses URIs', t => {
+  const uri = globalRegistery.get('rfc3986-uri');
+
+  t.true(uri.parse('http://example.org'));
+  t.true(uri.parse('https://example.org/'));
+  t.true(uri.parse('https://example.org/index.php?q=hello'));
+  t.true(uri.parse('https://example.org/search%20page.php?q=hello'));
+  t.true(uri.parse('https://example.org/search%20page.php?q=hello#content'));
+  t.true(uri.parse('git+ssh://user@server/project.git'));
+  t.false(uri.parse('https://example.org/search page.php?q=hello#content'));
 });
